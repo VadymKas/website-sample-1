7604a0e62f7f2582a03b64e612d2761169ae4c35
Vadym Kaschin <v.kaschin@gmail.com>
2026-04-27 16:34:21 +0300

    fix: fixed issue in Container

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,12 +5,12 @@ type Props = {
 	children: React.ReactNode
 }
 
-const Container: FC<Props> = ({className, children}) => {
-  return (
-	<div className={`container lg:max-w-6xl px-4 mx-auto ${}`}>
-		{children}
-	</div>
-  )
+const Container: React.FC<Props> = ({ className, children }) => {
+	return (
+		<div className={`container lg:max-w-6xl px-4 mx-auto ${className}`}>
+			{children}
+		</div>
+	)
 }
 
 export default Container
